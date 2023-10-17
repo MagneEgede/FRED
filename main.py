e0ad2d56a117032a0b34238eec94d4cafa3b0978
MagneEgede <magneegede@gmail.com>
2023-10-17 15:09:12 +0100

    text changes

--- main.py
+++ main.py
@@ -86,8 +86,8 @@ if user_menu == 'FQ Performance - Turbine Level':
             coll23.metric("AC/DC Variance", "0.2", "-0.2")
             coll24.metric("DC/AC Variance", "1.2", "+0.8")
 
-        st.text('The 2. unit located in the first series of the DC/AC is about break. '
-                'The most likely cause is humitidy.')
+            st.text('The 2. unit located in the first series of the DC/AC is about break. '
+                    'The most likely cause is humitidy.')
 
 
 
